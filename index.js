fe267aa447ff03eb7ff5d5e7aad74ed47e0069c5
Claudiu <clayarname4@gmail.com>
2023-06-13 20:36:59 +0300

    Restructure

--- index.js
+++ index.js
@@ -5,35 +5,39 @@ const io = require('socket.io')(httpServer, {
     }
 })
 const axios = require('axios').default
-var querystring = require('querystring');
-const http = require('http');
 const crypto = require('crypto');
+const { Worker } = require('node:worker_threads');
+
+
+const DEFAULT_TIME_CONTROL = 25000;
+const MAX_SEARCH_RANGE = 200;
+const STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+const worker = new Worker("./worker.js");
+worker.on("message", () => {
+    updateTimers();
+})
 
 let queue = [];
+let rooms = new Map();
+setInterval(findQueuePairs, 5000);
 
 io.on("connection", socket => {
-    console.log(socket.id)
-
-    socket.on("checkexisting", (params, callback) => {
+    socket.on("check-existing", (params, callback) => {
         socketExistingGame = checkUidInGame(params.userId);
         if (socketExistingGame) {
-            console.log('found game');
             callback(socketExistingGame);
         }
     })
 
-    socket.on("joinqueue", (params, callback) => {
+    socket.on("join-queue", (params, callback) => {
         if (checkUidInQueue(params.userId) == true) return;
-        queue.push({ id: socket.id, rating: params.rating, ratingOffset: 10, uid: params.userId });
-        console.log(queue);
+        queue.push({ id: socket.id, rating: params.rating, ratingOffset: 50, uid: params.userId });
         callback({ queueLength: queue.length })
     })
 
     socket.on("join-room", (params, callback) => {
-        console.log(socket.id + " joined " + params.room);
         socket.join(params.room);
         if (!rooms.get(params.room)) {
-            console.log("skipping");
             return
         };
         let clr = "spectator";
@@ -43,32 +47,50 @@ io.on("connection", socket => {
         if (rooms.get(params.room).blackId == params.uid) {
             clr = "black";
         }
-        console.log(rooms.get(params.room));
-        callback({ color: clr, room: rooms.get(params.room)});
+        callback({ color: clr, room: rooms.get(params.room) });
+    })
+
+    socket.on("make-move", params => {
+        if (!params.room) return;
+        if (!rooms.get(params.room)) return;
+        rooms.get(params.room).FEN = params.FEN;
+        rooms.get(params.room).pastPositions = params.pastPositions;
+        params.whiteTime = rooms.get(params.room).whiteTime;
+        params.blackTime = rooms.get(params.room).blackTime;
+        rooms.get(params.room).whiteTurn = !rooms.get(params.room).whiteTurn;
+        socket.to(params.room).emit("new-move", params);
+    })
+
+    socket.on("leave-queue", params => {
+        removeQueue(socket.id);
+    })
+
+    socket.on("game-end", params => {
+        endGame(params);
+    })
+
+    socket.on("send-message", params => {
+        rooms.get(params.room).chat_logs.push({ author: params.author, content: params.content });
+        socket.to(params.room).emit("receive-message", params);
+    })
+
+    socket.on("offer-draw", params => {
+        socket.to(params.room).emit("draw-offered");
     })
 
     socket.on("disconnect", reason => {
         removeQueue(socket.id);
-        console.log(reason);
     })
+})
 
-    socket.on("make move", params => {
-        if (!params.room) return;
-        rooms.get(params.room).FEN = params.FEN;
-        rooms.get(params.room).pastPositions = params.pastPositions;
-        socket.to(params.room).emit("new move", params);
-    })
+function endGame(params) {
+    if (!params.room) return;
+    if (!rooms.get(params.room)) return;
+    rooms.get(params.room).pastPositions.push(rooms.get(params.room).FEN);
+    io.to(params.room).emit("game-ended", params);
 
-    socket.on("leavequeue", params => {
-        console.log("leaving")
-        removeQueue(socket.id);
-    })
-
-    socket.on("gameend", params => {
-        if (!params.room) return;
-        rooms.get(params.room).pastPositions.push(rooms.get(params.room).FEN);
-        io.to(params.room).emit("gameended", params);
-        console.log({
+    axios.post("http://localhost:3000/game",
+        {
             id: params.room,
             result: params.status.result,
             FENS: JSON.stringify(rooms.get(params.room).pastPositions),
@@ -76,34 +98,25 @@ io.on("connection", socket => {
             white_id: rooms.get(params.room).whiteId,
             black_id: rooms.get(params.room).blackId,
         });
-        axios.post("http://localhost:3000/game",
-            {
-                id: params.room,
-                result: params.status.result,
-                FENS: JSON.stringify(rooms.get(params.room).pastPositions),
-                chat_logs: JSON.stringify(rooms.get(params.room).chat_logs),
-                white_id: rooms.get(params.room).whiteId,
-                black_id: rooms.get(params.room).blackId,
-            });
-        updateRatings(rooms.get(params.room).whiteId,rooms.get(params.room).blackId, params.status.result);
+
+    updateRatings(rooms.get(params.room).whiteId, rooms.get(params.room).blackId, params.status.result);
+    rooms.delete(params.room);
+}
+
+function updateTimers() {
+    rooms.forEach(room => {
+        if (room.whiteTurn) {
+            if (room.whiteTime >= 100)
+                room.whiteTime -= 100;
+        }
+        else {
+            if (room.blackTime >= 100)
+                room.blackTime -= 100;
+        }
+        if (room.whiteTime <= 0) endGame({ room: room.id, status: { result: "Black win", message: "Timeout" } })
+        if (room.blackTime <= 0) endGame({ room: room.id, status: { result: "White win", message: "Timeout" } });
     })
-
-    socket.on("send-message", params => {
-        console.log(params);
-        rooms.get(params.room).chat_logs.push({author: params.author, content: params.content});
-        socket.to(params.room).emit("receive-message", params);
-    })
-
-    socket.on("offer-draw", params => {
-        console.log(params);
-        socket.to(params.room).emit("draw-offered");
-    })
-
-})
-
-const matchmakeInterval = setInterval(findPairs, 5000);
-
-let rooms = new Map();
+}
 
 function checkUidInQueue(uid) {
     let found = false;
@@ -118,7 +131,6 @@ function removeQueue(socketId) {
     for (let i = 0; i < queue.length; i++) {
         if (queue[i].id == socketId) {
             queue.splice(i, 1);
-            console.log("removed from queue " + socketId);
         }
     }
 }
@@ -138,7 +150,7 @@ async function updateRatings(white_id, black_id, result) {
     let white_player = (await axios.get(`http://localhost:3000/user/id/${white_id}`)).data;
     let black_player = (await axios.get(`http://localhost:3000/user/id/${black_id}`)).data;
 
-    probabilityWhiteWin = 1 / ( 1 + Math.pow(10,((black_player.rating - white_player.rating)/400)));
+    probabilityWhiteWin = 1 / (1 + Math.pow(10, ((black_player.rating - white_player.rating) / 400)));
     probabilityBlackWin = 1 - probabilityWhiteWin;
 
     let res = 0.5;
@@ -147,44 +159,43 @@ async function updateRatings(white_id, black_id, result) {
         case "Draw": res = 0.5; break;
         case "Black win": res = 0; break;
     }
-    let newWhiteRating = white_player.rating + 20*(res - probabilityWhiteWin);
-    let newBlackRating = black_player.rating + 20*((1-res) - probabilityBlackWin);
+    let newWhiteRating = white_player.rating + 20 * (res - probabilityWhiteWin);
+    let newBlackRating = black_player.rating + 20 * ((1 - res) - probabilityBlackWin);
 
-    await axios.patch(`http://localhost:3000/user/id/${white_id}`,{
+    await axios.patch(`http://localhost:3000/user/id/${white_id}`, {
         id: white_id,
         rating: Math.round(newWhiteRating)
     })
 
-    await axios.patch(`http://localhost:3000/user/id/${black_id}`,{
+    await axios.patch(`http://localhost:3000/user/id/${black_id}`, {
         id: black_id,
         rating: Math.round(newBlackRating)
     })
 }
 
-function findPairs() {
+function findQueuePairs() {
     if (queue.length == 0) return;
     for (let i = 0; i < queue.length; i++) {
         for (let j = i + 1; j < queue.length; j++) {
-            console.log(`Comparing ${i}.${queue[i].id} to ${j}.${queue[j].id}`);
             if (Math.abs(queue[i].rating - queue[j].rating) < Math.min(queue[i].ratingOffset, queue[j].ratingOffset)) {
                 let id = crypto.randomUUID();
-                console.log(`Matching ${i}.${queue[i]} to ${j}.${queue[j]}`);
-                let startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
                 if (Math.random() > 0.5) {
-                    rooms.set(id, { id: id, whiteId: queue[i].uid, blackId: queue[j].uid, FEN: startingFEN, pastPositions: [], isOngoing: true, chat_logs: [] });
-                    io.to(queue[i].id).emit("new match", { roomId: id, opponentId: queue[j].uid, playingWhite: true });
-                    io.to(queue[j].id).emit("new match", { roomId: id, opponentId: queue[i].uid, playingWhite: false });
+                    rooms.set(id, { id: id, whiteId: queue[i].uid, blackId: queue[j].uid, FEN: STARTING_FEN, pastPositions: [], chat_logs: [], whiteTurn: true, whiteTime: DEFAULT_TIME_CONTROL, blackTime: DEFAULT_TIME_CONTROL });
+                    io.to(queue[i].id).emit("new-match", { roomId: id, opponentId: queue[j].uid, playingWhite: true });
+                    io.to(queue[j].id).emit("new-match", { roomId: id, opponentId: queue[i].uid, playingWhite: false });
                 } else {
-                    rooms.set(id, { id: id, whiteId: queue[j].uid, blackId: queue[i].uid, FEN: startingFEN, pastPositions: [], isOngoing: true, chat_logs: [] });
-                    io.to(queue[i].id).emit("new match", { roomId: id, opponentId: queue[j].uid, playingWhite: false });
-                    io.to(queue[j].id).emit("new match", { roomId: id, opponentId: queue[i].uid, playingWhite: true });
+                    rooms.set(id, { id: id, whiteId: queue[j].uid, blackId: queue[i].uid, FEN: STARTING_FEN, pastPositions: [], chat_logs: [], whiteTurn: true, whiteTime: DEFAULT_TIME_CONTROL, blackTime: DEFAULT_TIME_CONTROL });
+                    io.to(queue[i].id).emit("new-match", { roomId: id, opponentId: queue[j].uid, playingWhite: false });
+                    io.to(queue[j].id).emit("new-match", { roomId: id, opponentId: queue[i].uid, playingWhite: true });
                 }
             }
         }
-        if (queue[i])
-            queue[i].ratingOffset += 10;
+        if (queue[i]) {
+            if (queue[i].ratingOffset < MAX_SEARCH_RANGE) { 
+                queue[i].ratingOffset += 10; 
+            }
+        }
     }
-    //console.log(rooms);
 }
 
 httpServer.listen(2112, "192.168.1.2");
